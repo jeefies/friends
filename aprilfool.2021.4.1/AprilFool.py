0fe152626f842d106591709f8e81f94fcbebfc18
jeefies <jeefy163@163.com>
2021-03-31 22:57:29 +0800

    new at some details

--- aprilfool.2021.4.1/AprilFool.py
+++ aprilfool.2021.4.1/AprilFool.py
@@ -3,21 +3,32 @@ from random import randint
 import tkinter as tk
 import tkinter.font as tkf
 from tkinter import messagebox as msg
-from multiprocessing import Process
-from collections import deque
-#from threading import Thread
 
 root = tk.Tk()
 root.withdraw()
 
+font = tkf.nametofont('TkDefaultFont').copy()
+
 ans = msg.askyesno('Warning', 'Your computer is attack by this virus!\n' \
-        '为了保证此病毒会带个您更好的体验，\n' \
+        '为了保证此病毒会带给您更好的体验，\n' \
         '我们将会争取您的意见，是否继续')
 
 if not ans: # no
-    msg.showwarning('Bad Answer', 'You made a bad choice!\n您的电脑将会被慢慢侵蚀！')
+    title, message, bit = 'Bad Answer', 'You made a bad choice!\n3秒后您的电脑将会被慢慢侵蚀！', 'warning'
 else:
-    msg.showinfo('Nice Answer', '您的电脑将会被温柔的入侵！')
+    title, message, bit = 'Nice Answer', '3秒后您的电脑将会被温柔的入侵！', 'warning'
+
+winsize = root.winfo_screenwidth() // 2 - 125, root.winfo_screenheight() // 2 - 25
+msgtop = tk.Toplevel()
+msgtop.title(title)
+msgtop.geometry('250x50+%d+%d' % winsize)
+bit = tk.BitmapImage(bit)
+tk.Label(msgtop, bitmap=bit).pack(side='left', fill='y', anchor='center', padx=10)
+tk.Label(msgtop, {tk.Pack: dict(anchor='center', expand='yes'), 'text': message})
+msgtop.update()
+msgtop.resizable(0,0)
+time.sleep(3)
+msgtop.destroy()
 
 root.update()
 top = tk.Toplevel()
@@ -33,35 +44,21 @@ top.geometry(f"%dx%d+{rw - width // 2}+{rh - width//2}" % (width, height))
 top.update()
 
 frame = tk.Frame(top, bg='black')
-frame.pack(fill='both', expand='yes')
+frame.pack(fill='both', expand='yes', padx=1, pady=1)
 
-font = tkf.nametofont('TkDefaultFont').copy()
 font['size'] = height // 2
-label = tk.Label(frame, text='5', font=font, bg='black', fg='white')
+label = tk.Label(frame, text='5', font=font, bg='black', fg='white', justify='left')
 label.pack(expand='yes', anchor='center')
 
 top.update()
 
 w, h = root.winfo_screenwidth(), root.winfo_screenheight()
-def makemany():
-    start = time.time()
-    tops = deque()
-    while time.time() - start < 5:
-        top = tk.Toplevel()
-        top.overrideredirect(True)
-        tops.append(top)
-        top.geometry('100x50+%d+%d' % (randint(0, w - 100), randint(0, h - 50)))
-        top.update()
-        time.sleep(0.2)
-    for top in tops:
-        top.destroy()
 
-#Process(target=makemany).start()
 for i in range(4):
     width += aw + 1
     height += ah + 1
     time.sleep(1)
-    top.geometry(f"%dx%d+{rw - width // 2}+{rh - height//2}" % (width, height))
+    top.geometry(f"%dx%d+{rw - width // 2 - 1}+{rh - height//2 - 1}" % (width, height))
     top.update()
     label['text'] = str(4 - i)
     label.update()
@@ -75,25 +72,27 @@ time.sleep(1)
 
 label['text'] = '病毒成功入侵！'
 top.update()
-time.sleep(1)
+time.sleep(1.5)
 
-for i in range(2):
-    for j in range(4):
+for i in range(randint(2, 5)):
+    for j in range(randint(2, 5)):
         label['text'] = '电脑崩溃中' + '.' * j
         top.update()
-        time.sleep(0.5)
+        time.sleep(randint(5, 10) / 10)
 
 label['bg'] = 'blue'
 frame['bg'] = 'blue'
 label['fg'] = 'white'
-font['size'] = 14
+font['size'] = h // 15
 
-label['text'] = '检测到您电脑异常，未能找到异常原因     '
+label['text'] = '检测到您电脑异常，\n未能找到异常原因\u3000'
 top.update()
+time.sleep(5.5)
+
+label['justify'] = 'right'
+label['text'] = '愚人节，祝您开心！ '\
+        '\nJeefy@github.com'
+top.update()
+
 time.sleep(5)
-
-label['text'] = '愚人节，祝您开心！\nJeefy@github.com'
-top.update()
-
-time.sleep(4)
 top.destroy()
